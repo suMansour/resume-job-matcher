c05eaf7624ef514fe0deb7e568e77111b10f2bc4
suhaib Mansour <suhaib@bpc.com>
2025-04-06 14:57:24 +0300

    Initial Commit

---
+++ src/db_connect.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        dbname="your_db",
+        user="your_user",
+        password="your_password",
+        host="your_host",
+        port="your_port"
+    )
